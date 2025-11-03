241a5e6f29f5c695f495ae3749c8bc15643d2970
Agustin_laptop <agus.urien2@gmail.com>
2025-11-03 17:11:34 -0300

    wpp y removal de form

---
+++ src/components/Contacto/Pedido.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { FaWhatsapp } from "react-icons/fa6";
+import "./pedido.css";
+
+const Pedido = () => {
+  const [isJumping, setIsJumping] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsJumping(true);
+      // la animación dura 1.5s → después de eso la quitamos
+      setTimeout(() => setIsJumping(false), 1500);
+    }, 3500); // cada 3 segundos
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <a
+      href="https://wa.me/5491164610799"
+      target="_blank"
+      className={`stickyWhatsapp ${isJumping ? "jumping" : ""}`}
+    >
+      <p>Hace tu Pedido!</p>
+      <div class="logoWpp">
+        <FaWhatsapp />
+      </div>
+    </a>
+  );
+};
+
+export default Pedido;
