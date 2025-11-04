1df2c2c6344534fb03d8a18a95f4122da47b0302
Agustin_laptop <agus.urien2@gmail.com>
2025-11-04 13:03:41 -0300

    links nav

--- src/components/Hamburguesa/NavHamburguesa.jsx
+++ src/components/Hamburguesa/NavHamburguesa.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./navhamburguesa.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseSharp } from "react-icons/io5";
+import { FaInstagram, FaLinkedinIn, FaTiktok } from "react-icons/fa6";
 
 const NavHamburguesa = () => {
   const [open, setOpen] = useState(false);
@@ -16,6 +17,24 @@ const NavHamburguesa = () => {
           <a href="#Entregas">ZONAS DE ENTREGA</a>
           <a href="#QuienesSomos">NOSOTROS</a>
           <a href="#contacto">CONTACTO</a>
+
+          <div className="socials">
+            <a href="https://www.instagram.com/grupo.bonprix/" target="_blank">
+              <FaInstagram />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/grupo-bonprix/?originalSubdomain=ar"
+              target="_blank"
+            >
+              <FaLinkedinIn />
+            </a>
+            <a
+              href="https://www.tiktok.com/@grupobonprix?_r=1&_t=ZM-91139ZdqeTr"
+              target="_blank"
+            >
+              <FaTiktok />
+            </a>
+          </div>
         </div>
       )}
     </>
